3eca98ed2f3985a98f6c2ff7a6de36a0b08cdee9
blendolinarashiti <blendolina.rashiti@etu.unige.ch>
2022-09-28 13:01:36 +0200

    describe and test the function

--- rfile.R
+++ rfile.R
@@ -2,8 +2,11 @@
 # Title : R-project
 #===============================================================================
 
+# Addition function returning the sum of two numbers 
+
 addition = function(x,y){
   result=x+y
   return(result)
 }
 
+addition(2,3)
